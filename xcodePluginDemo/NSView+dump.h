8ab36c930a1454191249b537098b1cc9e85d48f1
汪洋 <igiuify@gmail.com>
2015-07-18 17:16:25 +0800

    xcode 6.4

---
+++ xcodePluginDemo/NSView+dump.h
@@ -0,0 +1,13 @@
+//
+//  NSView+dump.h
+//  xcodePluginDemo
+//
+//  Created by wangyang on 5/21/15.
+//  Copyright (c) 2015 com.wy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSView (dump)
+-(void)dumpWithIndent:(NSString *)indent;
+@end
--- xcodePluginDemo/xcodePluginDemo.m
+++ xcodePluginDemo/xcodePluginDemo.m
@@ -7,6 +7,7 @@
 //
 
 #import "xcodePluginDemo.h"
+#import "NSView+dump.h"
 
 static xcodePluginDemo *sharedPlugin;
 
@@ -38,27 +39,43 @@ static xcodePluginDemo *sharedPlugin;
     if (self = [super init]) {
         // reference to plugin's bundle, for resource access
         self.bundle = plugin;
+ 
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(applicationDidFinishLaunching:)
+                                                     name:NSApplicationDidFinishLaunchingNotification
+                                                   object:nil];
         
-        // Create menu items, initialize UI, etc.
-
-        // Sample Menu Item:
-        NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
-        if (menuItem) {
-            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
-            NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"Do Action" action:@selector(doMenuAction) keyEquivalent:@""];
-            [actionMenuItem setTarget:self];
-            [[menuItem submenu] addItem:actionMenuItem];
-        }
+       
     }
     return self;
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification*)noti {
+    NSMenuItem *menuItem = [[NSApp mainMenu] itemWithTitle:@"Edit"];
+    if (menuItem) {
+        //            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
+        NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"dump current window" action:@selector(doMenuAction) keyEquivalent:@""];
+        [actionMenuItem setTarget:self];
+        [[menuItem submenu] addItem:actionMenuItem];
+    }
+    
+    NSMenuItem *helpMenuItem = [[NSApp mainMenu] itemWithTitle:@"Help"];
+    if (helpMenuItem) {
+        //            [[menuItem submenu] addItem:[NSMenuItem separatorItem]];
+        NSMenuItem *actionMenuItem = [[NSMenuItem alloc] initWithTitle:@"dump current window" action:@selector(doMenuAction) keyEquivalent:@""];
+        [actionMenuItem setTarget:self];
+        [[helpMenuItem submenu] addItem:actionMenuItem];
+    }
+    
 }
 
 // Sample Action, for menu item:
 - (void)doMenuAction
 {
-    NSAlert *alert = [[NSAlert alloc] init];
-    [alert setMessageText:@"Hello, World"];
-    [alert runModal];
+//    NSAlert *alert = [[NSAlert alloc] init];
+//    [alert setMessageText:@"Hello, World"];
+//    [alert runModal];
+    [[[NSApp mainWindow] contentView] dumpWithIndent:@""];
 }
 
 - (void)dealloc
